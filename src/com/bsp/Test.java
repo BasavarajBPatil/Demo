494eee83449258cfb2bfcd7828c7b17922ed1300
BasavarajBPatil <patilbasavaraj03@gmail.com>
2024-03-19 14:23:23 +0530

    anthore two lines added

--- src/com/bsp/Test.java
+++ src/com/bsp/Test.java
@@ -2,8 +2,10 @@ package com.bsp;
 
 public class Test 
 {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("simple hello message");
-
+	System.out.println("stamt2");
+System.out.println("stmt3");
 }
 }
